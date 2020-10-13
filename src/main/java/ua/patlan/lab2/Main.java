1a09ff374a065a47aff61bbcd04f03d1385aa689
olehPatlan <olehpatlan@gmail.com>
2020-10-13 10:36:40 +0300

    refactoring Main.java

--- src/main/java/ua/patlan/lab2/Main.java
+++ src/main/java/ua/patlan/lab2/Main.java
@@ -8,25 +8,28 @@ import ua.patlan.lab2.service.BookService;
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(HibernateConfig.class);
-        Book book=new Book();
-        book.setName("aaaa");
-        book.setAuthor("aaaa");
-
         Book book1=new Book();
-        book1.setName("aaaa2");
-        book1.setAuthor("aaaa2");
+        book1.setName("aaaa");
+        book1.setAuthor("aaaa");
+
+        Book book2=new Book();
+        book2.setName("aaaa2");
+        book2.setAuthor("aaaa2");
 
         BookService bookDAO=(BookService) context.getBean("bookServiceImpl");
-        bookDAO.save(book);
         bookDAO.save(book1);
+        bookDAO.save(book2);
+        System.out.println("bookDAO.findAll()");
         System.out.println(bookDAO.findAll());
 
-        book.setAuthor("1234");
-        bookDAO.update(book);
 
+        System.out.println("Setting author book1");
+        book1.setAuthor("1234");
+        bookDAO.update(book1);
         System.out.println(bookDAO.findAll());
 
-        bookDAO.delete(book);
+        System.out.println("Deleting book1");
+        bookDAO.delete(book1);
         System.out.println(bookDAO.findAll());
     }
 }
